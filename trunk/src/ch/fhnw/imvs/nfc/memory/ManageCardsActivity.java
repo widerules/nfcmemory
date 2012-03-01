d788879ee85266caa29c9b460c4cc215a5d35c75
jonas.lauener@gmail.com <jonas.lauener@gmail.com@cc08f0d1-ef0a-81ab-6129-bb5789532730>
2012-03-01 08:49:36 +0000

    added request before quiting game

--- trunk/src/ch/fhnw/imvs/nfc/memory/ManageCardsActivity.java
+++ trunk/src/ch/fhnw/imvs/nfc/memory/ManageCardsActivity.java
@@ -1,6 +1,5 @@
 /*
- * Copyright (c) 2012 Fachhochschule Nordwestschweiz (FHNW)
- * All Rights Reserved. 
+ * Copyright (c) 2012 Fachhochschule Nordwestschweiz (FHNW) All Rights Reserved.
  */
 
 package ch.fhnw.imvs.nfc.memory;
@@ -67,8 +66,7 @@ public class ManageCardsActivity
 
         requestWindowFeature(Window.FEATURE_CUSTOM_TITLE);
         setContentView(R.layout.memorize_cards);
-        getWindow().setFeatureInt(Window.FEATURE_CUSTOM_TITLE,
-            R.layout.custom_title);
+        getWindow().setFeatureInt(Window.FEATURE_CUSTOM_TITLE, R.layout.custom_title);
 
         TextView activityTitle = (TextView) findViewById(R.id.left_text);
         activityTitle.setText(R.string.manage_cards_activity_title);
@@ -76,9 +74,7 @@ public class ManageCardsActivity
 
         statusBar = (TextView) findViewById(R.id.status_bar);
         nfcAdapter = NfcAdapter.getDefaultAdapter(this);
-        nfcPendingIntent =
-            PendingIntent.getActivity(this, 0, new Intent(this, getClass())
-                .addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP), 0);
+        nfcPendingIntent = PendingIntent.getActivity(this, 0, new Intent(this, getClass()).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP), 0);
         vibrator = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
         cards = new ArrayList<String>();
         animationFadeIn = AnimationUtils.loadAnimation(this, R.anim.slide_in);
@@ -96,12 +92,8 @@ public class ManageCardsActivity
         super.onResume();
         try
         {
-            cards =
-                JSONSharedPreferences.loadStringList(this,
-                    SharedPreferencesKeys.PREF_NAME,
-                    SharedPreferencesKeys.CARDS_KEY);
-            cardCounter.setText(cards.size()
-                + getString(R.string.number_of_cards));
+            cards = JSONSharedPreferences.loadStringList(this, SharedPreferencesKeys.PREF_NAME, SharedPreferencesKeys.CARDS_KEY);
+            cardCounter.setText(cards.size() + getString(R.string.number_of_cards));
         }
         catch (JSONException e)
         {
@@ -131,37 +123,30 @@ public class ManageCardsActivity
         else
         {
             // analyze what is to do to solve this problem
-            final CharSequence[] possibilities =
-                getResources().getStringArray(
-                    R.array.impossible_number_of_cards_dialog);
+            final CharSequence[] possibilities = getResources().getStringArray(R.array.impossible_number_of_cards_dialog);
 
-            new AlertDialog.Builder(this)
-                .setItems(possibilities, new OnClickListener()
+            new AlertDialog.Builder(this).setItems(possibilities, new OnClickListener()
+            {
+                @Override
+                public void onClick(DialogInterface dialog, int which)
                 {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which)
+                    switch (which)
                     {
-                        switch (which)
-                        {
-                        // user selected to delete the last card
-                        case 0:
-                            cards.remove(cards.size() - 1);
-                            saveCards();
-                            finish();
-                            break;
-                        // user selects add one card
-                        case 1:
-                            break;
-                        default:
-                            finish();
-                            break;
-                        }
+                    // user selected to delete the last card
+                    case 0:
+                        cards.remove(cards.size() - 1);
+                        saveCards();
+                        finish();
+                        break;
+                    // user selects add one card
+                    case 1:
+                        break;
+                    default:
+                        finish();
+                        break;
                     }
-                })
-                .setTitle(
-                    String.format(
-                        getString(R.string.impossible_number_of_cards_title),
-                        cards.size())).show();
+                }
+            }).setTitle(String.format(getString(R.string.impossible_number_of_cards_title), cards.size())).show();
 
         }
     }
@@ -180,30 +165,16 @@ public class ManageCardsActivity
         switch (item.getItemId())
         {
         case R.id.delete_memorized_cards:
-            new AlertDialog.Builder(this)
-                .setTitle(R.string.request_delete_all_cards)
-                .setIcon(android.R.drawable.ic_dialog_alert)
-                .setPositiveButton(android.R.string.ok,
-                    new DialogInterface.OnClickListener()
-                    {
-                        public void onClick(DialogInterface dialog,
-                            int whichButton)
-                        {
-                            // User clicked OK
-                            cards.clear();
-                            saveCards();
-                            updateView();
-                        }
-                    })
-                .setNegativeButton(android.R.string.cancel,
-                    new DialogInterface.OnClickListener()
-                    {
-                        public void onClick(DialogInterface dialog,
-                            int whichButton)
-                        {
-                            // User clicked Cancel
-                        }
-                    }).show();
+            new AlertDialog.Builder(this).setTitle(R.string.request_delete_all_cards).setIcon(android.R.drawable.ic_dialog_alert).setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener()
+            {
+                public void onClick(DialogInterface dialog, int whichButton)
+                {
+                    // User clicked OK
+                    cards.clear();
+                    saveCards();
+                    updateView();
+                }
+            }).setNegativeButton(android.R.string.cancel, null).show();
             return true;
         default:
             return super.onOptionsItemSelected(item);
@@ -212,13 +183,11 @@ public class ManageCardsActivity
     }
 
     @Override
-    public void onCreateContextMenu(ContextMenu menu, View v,
-        ContextMenuInfo menuInfo)
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenuInfo menuInfo)
     {
         if (v.getId() == R.id.cards_grid)
         {
-            AdapterView.AdapterContextMenuInfo info =
-                (AdapterView.AdapterContextMenuInfo) menuInfo;
+            AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) menuInfo;
             MenuInflater inflater = getMenuInflater();
             inflater.inflate(R.menu.memorize_cards_context, menu);
             menu.setHeaderTitle(cards.get(info.position));
@@ -228,36 +197,27 @@ public class ManageCardsActivity
     @Override
     public boolean onContextItemSelected(MenuItem item)
     {
-        final AdapterView.AdapterContextMenuInfo info =
-            (AdapterView.AdapterContextMenuInfo) item.getMenuInfo();
+        final AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) item.getMenuInfo();
 
         switch (item.getItemId())
         {
         case R.id.delete_card:
-            new AlertDialog.Builder(this)
-                .setTitle(R.string.request_delete_card)
-                .setIcon(android.R.drawable.ic_dialog_alert)
-                .setPositiveButton(android.R.string.ok,
-                    new DialogInterface.OnClickListener()
-                    {
-                        public void onClick(DialogInterface dialog,
-                            int whichButton)
-                        {
-                            // User clicked OK
-                            cards.remove(info.position);
-                            saveCards();
-                            updateView();
-                        }
-                    })
-                .setNegativeButton(android.R.string.cancel,
-                    new DialogInterface.OnClickListener()
-                    {
-                        public void onClick(DialogInterface dialog,
-                            int whichButton)
-                        {
-                            // User clicked Cancel
-                        }
-                    }).show();
+            new AlertDialog.Builder(this).setTitle(R.string.request_delete_card).setIcon(android.R.drawable.ic_dialog_alert).setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener()
+            {
+                public void onClick(DialogInterface dialog, int whichButton)
+                {
+                    // User clicked OK
+                    cards.remove(info.position);
+                    saveCards();
+                    updateView();
+                }
+            }).setNegativeButton(android.R.string.cancel, new DialogInterface.OnClickListener()
+            {
+                public void onClick(DialogInterface dialog, int whichButton)
+                {
+                    // User clicked Cancel
+                }
+            }).show();
             break;
         }
         return true;
@@ -269,11 +229,10 @@ public class ManageCardsActivity
         String item = getIdFromNfcTag(intent);
         if (item != null && item != "00")
         {
-            //if not already scanned tag
+            // if not already scanned tag
             if (!cards.contains(item))
             {
-                statusBar.setText(String.format(getString(R.string.tag_added),
-                    item));
+                statusBar.setText(String.format(getString(R.string.tag_added), item));
                 vibrator.vibrate(400);
                 cards.add(item);
                 saveCards();
@@ -284,7 +243,7 @@ public class ManageCardsActivity
                 statusBar.setText(getString(R.string.tag_already_existing));
                 vibrator.vibrate(VIBRATOR_ERROR_PATTERN, -1);
             }
-            //set info text back after some delay
+            // set info text back after some delay
             new Handler().postDelayed(new Runnable()
             {
                 @Override
@@ -300,9 +259,7 @@ public class ManageCardsActivity
     {
         String code = null;
         String action = intent.getAction();
-        if (NfcAdapter.ACTION_NDEF_DISCOVERED.equals(action)
-            || NfcAdapter.ACTION_TAG_DISCOVERED.equals(action)
-            || NfcAdapter.ACTION_TECH_DISCOVERED.equals(action))
+        if (NfcAdapter.ACTION_NDEF_DISCOVERED.equals(action) || NfcAdapter.ACTION_TAG_DISCOVERED.equals(action) || NfcAdapter.ACTION_TECH_DISCOVERED.equals(action))
         {
             byte[] p1 = intent.getByteArrayExtra(NfcAdapter.EXTRA_ID);
             code = getHex(p1);
@@ -315,9 +272,7 @@ public class ManageCardsActivity
      */
     private void saveCards()
     {
-        JSONSharedPreferences.saveJSONArray(this,
-            SharedPreferencesKeys.PREF_NAME, SharedPreferencesKeys.CARDS_KEY,
-            new JSONArray(cards));
+        JSONSharedPreferences.saveJSONArray(this, SharedPreferencesKeys.PREF_NAME, SharedPreferencesKeys.CARDS_KEY, new JSONArray(cards));
     }
 
     private void updateView()
@@ -338,16 +293,16 @@ public class ManageCardsActivity
         final StringBuilder hex = new StringBuilder(2 * raw.length);
         for (final byte b : raw)
         {
-            hex.append(HEXES.charAt((b & 0xF0) >> 4)).append(
-                HEXES.charAt((b & 0x0F)));
+            hex.append(HEXES.charAt((b & 0xF0) >> 4)).append(HEXES.charAt((b & 0x0F)));
         }
         return hex.toString();
     }
 
     /**
      * to provide the cards for the gridView
+     * 
      * @author jonas.lauener
-     *
+     * 
      */
     public class CardAdapter
         extends BaseAdapter
